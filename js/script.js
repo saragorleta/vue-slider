0c575f20b1f01c573384019b4cbc5474091a2651
Sara <saragorleta@yahoo.com>
2021-01-26 17:24:44 +0100

    add html + css

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var slider= new Vue({
+  el:'#app',
+  data: {
+    immagini: [
+      'img/img1.jpg',
+      'img/img2.jpg',
+      'img/img3.jpg',
+      'img/img4.jpg',
+      'img/img5.jpg',
+    ]
+  }
+});
