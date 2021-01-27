4c101f1de9231ea4531a4f2e2dc0b44b25dc3aff
Sara <saragorleta@yahoo.com>
2021-01-27 16:08:16 +0100

    corretto errori

--- js/script.js
+++ js/script.js
@@ -10,21 +10,29 @@ var slider= new Vue({
       'img/img5.jpg'
     ]
   },
+  // !!quando devo andare a prendere delle variabili in data devo andare
+  // ad utilizzare il this!!
   methods: {
     nextPhoto(){
-      if(this.contatore == (immagini.length - 1)){
+      if(this.contatore == (this.immagini.length - 1)){
                     // immagini.length - 1 cosi che parta da 0 e arrivi a 4
-      this.contatore=0;//quando arriva a 5 cambia con 0
-    } else{
-      this.contatore++; //altrimenti continua ad andare avanti il contatore
-    }
+        this.contatore=0;//quando arriva a 5 cambia con 0
+      } else{
+        this.contatore++; //altrimenti continua ad andare avanti il contatore
+      }
+    },
     prevPhoto(){
       if(this.contatore < 1) {
-        this.counter = this.immagini.length - 1; //il contatore deve essere a 5
-      }else {
+        this.contatore = this.immagini.length - 1; //il contatore deve essere a 5
+      } else {
         this.contatore--; //altrimenti continua ad andare indietro il contatore
       }
+    }
   },
+  //questa funzione serve per andare a cliccare sul pallino,
+  // facendolo corrispondere alla foto
+  cambiaFoto(indice){
+    this.contatore=indice;
+    // gli diciamo che il contatore deve corrispondere all'indice
+  }
 });
-// !!quando devo andare a prendere delle variabili in data devo andare
-// ad utilizzare il this!!
